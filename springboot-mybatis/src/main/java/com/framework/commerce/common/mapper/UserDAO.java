681d04b027e8a93d6383a12937e1ba0d7c30f1b4
ksofting <527018873@qq.com>
2017-04-21 16:28:48 +0800

    删除

--- springboot-mybatis/src/main/java/com/framework/commerce/common/mapper/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.framework.commerce.common.mapper;
-
-import com.framework.commerce.common.vo.UserVo;
-import com.framework.common.mapper.UserMapper;
-
-public interface UserDAO extends UserMapper{
-    public UserVo selectUserCount(String userName);
-}
